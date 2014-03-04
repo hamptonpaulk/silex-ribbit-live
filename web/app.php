2421592d14b7d25530d0a6ffc34f520a9a7fe336
Hampton Paulk <hampton@teamtreehouse.com>
2014-03-04 14:14:33 -0500

    add twig, and index file

--- web/app.php
+++ web/app.php
@@ -5,6 +5,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
 $app->get('/hello', function() {
     return 'Hello!';
 });
@@ -14,7 +19,7 @@ $app->get('/hello/{name}', function($name) {
 });
 
 $app->get('/', function() use ($app) {
-    return $app->redirect('/hello');
+    return $app['twig']->render('index.php.twig');
 });
 
 $app->run();
